54a6e15432d7ff55874807eaed56e307568c6a85
kazumaneet <mekbibtare@gmail.com>
2023-10-09 12:54:50 +0300

    Create a projects module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import createTodo from "./todo";
 import createProject from "./project";
+import projects from "./projects";
 
 var project1 = createProject('First Project');
 var todo1 = createTodo("Buy Groceries", "Make a list of essential groceries and go shopping for the week.", new Date(2023, 10, 15), "Medium", false);
@@ -11,17 +12,44 @@ project1.addTodo(todo3);
 var todo4 = createTodo("Exercise", "Go for a 30-minute jog in the evening to stay active.", new Date(2023, 10, 10), "Medium", false);
 project1.addTodo(todo4);
 
-console.log(project1.getId());
-console.log(project1.getTitle());
-project1.setTitle('new title');
-console.log(project1.getTitle());
-project1.deleteToDo(todo2.getId());
-project1.getTodoList().forEach(todo =>{
-  console.log(todo.getId());
-  console.log(todo.getTitle());
-  console.log(todo.getDueDate());
+// Create a new project called 'Second Project'
+var project2 = createProject('Second Project');
+
+var todo5 = createTodo("Research for Report", "Gather information and data for the upcoming quarterly report.", new Date(2023, 10, 18), "High", false);
+project2.addTodo(todo5);
+
+var todo6 = createTodo("Pay Bills", "Pay monthly utility bills and rent.", new Date(2023, 10, 5), "Medium", false);
+project2.addTodo(todo6);
+
+var todo7 = createTodo("Book Flight Tickets", "Book flight tickets for the upcoming vacation.", new Date(2023, 11, 1), "Medium", false);
+project2.addTodo(todo7);
+
+var todo8 = createTodo("Read a Chapter", "Read one chapter of the new book.", new Date(2023, 10, 14), "Low", false);
+project2.addTodo(todo8);
+
+// Create a new project called 'Third Project'
+var project3 = createProject('Third Project');
+
+var todo9 = createTodo("Write Blog Post", "Start drafting a blog post on the latest industry trends.", new Date(2023, 10, 22), "High", false);
+project3.addTodo(todo9);
+
+var todo10 = createTodo("Attend Webinar", "Register and attend the webinar on marketing strategies.", new Date(2023, 10, 8), "Medium", false);
+project3.addTodo(todo10);
+
+var todo11 = createTodo("Plan Birthday Party", "Plan a surprise birthday party for a friend.", new Date(2023, 10, 25), "High", false);
+project3.addTodo(todo11);
+
+var todo12 = createTodo("Learn Guitar Chords", "Practice and learn new guitar chords for the song.", new Date(2023, 10, 16), "Low", false);
+project3.addTodo(todo12);
+
+projects.addProject(project1);
+projects.addProject(project2);
+projects.addProject(project3);
+projects.deleteProject(project1.getId());
+projects.getProjectList().forEach(project => {
+  console.log(project.getId());
+  console.log(project.getTitle());
 })
 
-var foundTodo = project1.findTodo(`${todo3.getId()}`);
-console.log(foundTodo.getTitle() + foundTodo.getId());
+console.log(projects.findProject(project2.getId()).getTitle());
 
---
+++ src/projects.js
@@ -0,0 +1,23 @@
+var projects = (() => {
+  var projectList = [];
+
+  function getProjectList() {
+    return projectList;
+  }
+
+  function addProject(project) {
+    projectList.push(project);
+  }
+
+  function findProject(id) {
+    return projectList.find(project => project.getId() == id);
+  }
+
+  function deleteProject(id) {
+    projectList = projectList.filter(project => project.getId() != id);
+  }
+
+  return {getProjectList, addProject, findProject, deleteProject};
+})();
+
+export default projects;
